75b411cb6edf9af388af5081119b8ec9d9a42f6b
Kamalesh9787 <kamaleshking957@gmail.com>
2024-09-29 14:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "25599491"))
 API_HASH = environ.get("API_HASH", "c8e3c0561cf148a6504f27b111fc3698")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://dl.kpsbots.workers.dev/dl/66d3fad6210c4d7ce3030606')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/ac2d4563862e8fd3b13c6.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5983189506').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "MiniFiles_Bot") # without @
 PORT = environ.get("PORT", "8080")
