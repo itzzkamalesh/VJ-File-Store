c1f0e41e4cc8275ddfc2718ad14c63345aebd5a9
Kamalesh9787 <kamaleshking957@gmail.com>
2024-10-02 21:07:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,6 +25,17 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
